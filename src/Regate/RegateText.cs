b1fd6eb09c3a47cb0c2b67fb2cad47b90ad4ec4f
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-09-18 17:41:05 +0430

    clean up RegateText

--- src/Regate/RegateText.cs
+++ src/Regate/RegateText.cs
@@ -6,22 +6,22 @@ namespace Regate
     public static partial class Regate
     {
         public static string Text(string name) =>
-            _Build(new RegateTextProps(name));
+            _Build(new TextProps(name));
 
         public static string Text(string name, bool isRequired) =>
-            _Build(new RegateTextProps(name, isRequired));
+            _Build(new TextProps(name, isRequired));
 
         public static string Text(string name, string value) =>
-            _Build(new RegateTextProps(name, value));
+            _Build(new TextProps(name, value));
 
         public static string Text(string name, string value, bool isRequired) =>
-            _Build(new RegateTextProps(name, value, isRequired));
+            _Build(new TextProps(name, value, isRequired));
 
-        public static string Text(RegateTextProps props) =>
-            _Build(props);
+        public static string Text(string name, TextOptions options) =>
+            _Build(new TextProps(name, options));
 
         
-        private static string _Build(RegateTextProps props)
+        private static string _Build(TextProps props)
         {
             var value = WebUtility.HtmlEncode(props.Value);
 
@@ -34,37 +34,48 @@ namespace Regate
                 {(props.IsRequired ? " required='required' " : "")}
             />";
         }
+
+        public class TextOptions
+        {
+            public string Placeholder { get; set; }
+        }
+
+        private class TextProps
+        {
+            public TextProps()
+            {
+            }
+
+            public TextProps(string name)
+            {
+                Name = name;
+            }
+
+            public TextProps(string name, string value) : this(name)
+            {
+                Value = value;
+            }
+
+            public TextProps(string name, bool isRequired) : this(name)
+            {
+                IsRequired = isRequired;
+            }
+
+            public TextProps(string name, TextOptions options) : this(name)
+            {
+                this.Placeholder = options.Placeholder;
+            }
+
+            public TextProps(string name, string value, bool isRequired) : this(name, value)
+            {
+                IsRequired = isRequired;
+            }
+
+            public string Name { get; set; }
+            public string Value { get; set; }
+            public string Placeholder { get; set; }
+            public bool IsRequired { get; set; }
+        }
     }
 
-    public class RegateTextProps
-    {
-        public RegateTextProps()
-        {
-        }
-
-        public RegateTextProps(string name)
-        {
-            Name = name;
-        }
-
-        public RegateTextProps(string name, string value) : this(name)
-        {
-            Value = value;
-        }
-
-        public RegateTextProps(string name, bool isRequired) : this(name)
-        {
-            IsRequired = isRequired;
-        }
-
-        public RegateTextProps(string name, string value, bool isRequired) : this(name, value)
-        {
-            IsRequired = isRequired;
-        }
-
-        public string Name { get; set; }
-        public string Value { get; set; }
-        public string Placeholder { get; set; }
-        public bool IsRequired { get; set; }
-    }
 }
