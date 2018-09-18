d436c1eb567219e022d7e5c64ae8ec9a9d477c79
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-09-18 17:19:46 +0430

    finalize Regate.Text

--- src/Regate/RegateText.cs
+++ src/Regate/RegateText.cs
@@ -3,30 +3,48 @@ using System.Net;
 
 namespace Regate
 {
-    public static class RegateText
+    public static partial class Regate
     {
-        public static string Build(
-            string name = "",
-            string value = "",
-            bool isRequired = false,
-            string placeholder = ""
-        )
+        public static string Text(string name)
         {
-            value = WebUtility.HtmlEncode(value);
+            var props = new RegateTextProps(name);
+            return _Build(props);
+        }
+
+        public static string Text(string name, string value)
+        {
+            var props = new RegateTextProps(name, value);
+            return _Build(props);
+        }
+
+        private static string _Build(RegateTextProps props)
+        {
+            var value = WebUtility.HtmlEncode(props.Value);
 
             return $@"<input
-                name='{name}'
                 type='text'
+                name='{props.Name}'
                 class='form-control'
-                placeholder='{placeholder}'
+                placeholder='{props.Placeholder}'
                 value='{value}'
-                {(isRequired ? " required='required' " : "")}
+                {(props.IsRequired ? " required='required' " : "")}
             />";
         }
     }
 
     public class RegateTextProps
     {
+        public RegateTextProps(string name)
+        {
+            Name = name;
+        }
+
+        public RegateTextProps(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
         public string Name { get; set; }
         public string Value { get; set; }
         public string Placeholder { get; set; }
