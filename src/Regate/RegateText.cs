a97b36dc8b512c748b83ff224ae7b8ff05fc9d0f
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-09-18 17:03:19 +0430

    first steps to create RegateText

--- src/Regate/RegateText.cs
+++ src/Regate/RegateText.cs
@@ -1,8 +1,35 @@
 ﻿using System;
+using System.Net;
 
 namespace Regate
 {
-    public class Class1
+    public static class RegateText
     {
+        public static string Build(
+            string name = "",
+            string value = "",
+            bool isRequired = false,
+            string placeholder = ""
+        )
+        {
+            value = WebUtility.HtmlEncode(value);
+
+            return $@"<input
+                name='{name}'
+                type='text'
+                class='form-control'
+                placeholder='{placeholder}'
+                value='{value}'
+                {(isRequired ? " required='required' " : "")}
+            />";
+        }
+    }
+
+    public class RegateTextProps
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Placeholder { get; set; }
+        public bool IsRequired { get; set; }
     }
 }
