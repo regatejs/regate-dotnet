e716f8669054b0753e5448ed5c2011d122efbfc0
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-09-22 13:21:41 +0330

    introduce UploaderController

---
+++ Regate.MVC/Controllers/UploaderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Regate.MVC.Controllers
+{
+    public class UploaderController : Controller
+    {
+        public ActionResult File(string field = "")
+        {
+            ViewBag.Field = field;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(string field, IFormFile file)
+        {
+            ViewBag.Field = field;
+            ViewBag.FileName = "uploaded-file-name.pdf";
+
+            return View();
+        }
+    }
+}
