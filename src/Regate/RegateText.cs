1be36f8c71f9195455ae33d4704703122fb6a057
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-09-22 09:40:19 +0330

    add other constructors for RegateText

--- src/Regate/RegateText.cs
+++ src/Regate/RegateText.cs
@@ -3,25 +3,33 @@ using System.Net;
 
 namespace Regate
 {
-    public static partial class Regate
+    public static class RegateText
     {
-        public static string Text(string name) =>
-            _Build(new TextProps(name));
+        public static string Build(string name) =>
+            _Build(new Props(name));
 
-        public static string Text(string name, bool isRequired) =>
-            _Build(new TextProps(name, isRequired));
+        public static string Build(string name, bool isRequired) =>
+            _Build(new Props(name, isRequired));
 
-        public static string Text(string name, string value) =>
-            _Build(new TextProps(name, value));
+        public static string Build(string name, string value) =>
+            _Build(new Props(name, value));
 
-        public static string Text(string name, string value, bool isRequired) =>
-            _Build(new TextProps(name, value, isRequired));
+        public static string Build(string name, string value, bool isRequired) =>
+            _Build(new Props(name, value, isRequired));
 
-        public static string Text(string name, TextOptions options) =>
-            _Build(new TextProps(name, options));
+        public static string Build(string name, Options options) =>
+            _Build(new Props(name, options));
 
-        
-        private static string _Build(TextProps props)
+        public static string Build(string name, bool isRequired, Options options) =>
+            _Build(new Props(name, isRequired, options));
+
+        public static string Build(string name, string value, Options options) =>
+            _Build(new Props(name, value, options));
+
+        public static string Build(string name, string value, bool isRequired, Options options) =>
+            _Build(new Props(name, value, isRequired, options));
+
+        private static string _Build(Props props)
         {
             var value = WebUtility.HtmlEncode(props.Value);
 
@@ -35,36 +43,51 @@ namespace Regate
             />";
         }
 
-        public class TextOptions
+        public class Options
         {
             public string Placeholder { get; set; }
         }
 
-        private class TextProps
+        private class Props
         {
-            public TextProps(string name)
+            public Props(string name)
             {
                 Name = name;
             }
 
-            public TextProps(string name, string value) : this(name)
+            public Props(string name, string value) : this(name)
             {
                 Value = value;
             }
 
-            public TextProps(string name, bool isRequired) : this(name)
+            public Props(string name, bool isRequired) : this(name)
             {
                 IsRequired = isRequired;
             }
 
-            public TextProps(string name, TextOptions options) : this(name)
+            public Props(string name, Options options) : this(name)
             {
                 Placeholder = options.Placeholder;
             }
 
-            public TextProps(string name, string value, bool isRequired) : this(name, value)
+            public Props(string name, string value, bool isRequired) : this(name, value)
             {
                 IsRequired = isRequired;
+            }
+
+            public Props(string name, bool isRequired, Options options) : this(name, isRequired)
+            {
+                Placeholder = options.Placeholder;
+            }
+
+            public Props(string name, string value, Options options) : this(name, value)
+            {
+                Placeholder = options.Placeholder;
+            }
+
+            public Props(string name, string value, bool isRequired, Options options) : this(name, value, isRequired)
+            {
+                Placeholder = options.Placeholder;
             }
 
             public string Name { get; }
