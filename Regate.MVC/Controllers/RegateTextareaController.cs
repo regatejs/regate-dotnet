d7eb9fd5ff157a5f785ff41527ee6e641d9b8eab
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-09-30 12:11:48 +0330

    implement early version of RegateTextarea

---
+++ Regate.MVC/Controllers/RegateTextareaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Regate.MVC.Controllers
+{
+    public class RegateTextareaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Regate/RegateTextarea/RegateTextareaBuild.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+
+namespace Regate
+{
+    public static partial class RegateTextarea
+    {
+        private static string _Build(Props props)
+        {
+            var uniqueId = $"RegateTextarea__{props.Name}__{Guid.NewGuid().ToString().Replace("-", "")}";
+
+            var value = WebUtility.HtmlEncode(props.Value);
+
+            return $@"
+                <script>document.write(Regate.RegateTextarea.markup('{uniqueId}'))</script>
+
+                <script>
+                    Regate.RegateTextarea.init({{
+                        id: '{uniqueId}',
+                        name: '{props.Name}',
+                        value: '{value}',
+                        isRequired: {props.IsRequired.ToString().ToLower()},
+                    }});
+                </script>
+            ";
+        }
+
+    }
+
+}
