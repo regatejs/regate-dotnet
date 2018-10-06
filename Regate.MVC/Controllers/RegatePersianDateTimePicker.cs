228454917c7f84ba4ac79c92bf7fc472b67c3426
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-10-06 16:02:40 +0330

    implement basic version of persian date time picker

---
+++ Regate.MVC/Controllers/RegatePersianDateTimePicker.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Regate.MVC.Controllers
+{
+    public class RegatePersianDateTimePicker : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
