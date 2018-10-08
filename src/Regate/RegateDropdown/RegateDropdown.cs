d30bee2ed08bea983eba71e4857152d1c411991d
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-10-08 15:00:04 +0330

    create javascript options from KeyValuePair in CSharp

---
+++ src/Regate/RegateDropdown/RegateDropdown.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace Regate
+{
+    public static partial class RegateDropdown
+    {
+        public static string Build(string name) =>
+            _Build(new Props(name));
+
+        public static string Build(string name, bool isRequired) =>
+            _Build(new Props(name, isRequired));
+
+        public static string Build(string name, int? value) =>
+            _Build(new Props(name, value));
+
+        public static string Build(string name, int? value, bool isRequired) =>
+            _Build(new Props(name, value, isRequired));
+
+    }
+
+}
---
+++ src/Regate/RegateDropdown/RegateDropdownBuild.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Encodings.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Regate
+{
+    public static partial class RegateDropdown
+    {
+        private static string _Build(Props props)
+        {
+            var uniqueId = $"RegateDropdown__{props.Name}__{Guid.NewGuid().ToString().Replace("-", "")}";
+
+            var options = new List<KeyValuePair<int, string>>
+            {
+                new KeyValuePair<int, string>(1, "سیب"),
+                new KeyValuePair<int, string>(2, "پرتقال"),
+                new KeyValuePair<int, string>(3, "هلو"),
+            };
+
+            var optionsJsonString = JsonConvert.SerializeObject(options,
+                new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+
+            return $@"
+                <script>document.write(Regate.RegateDropdown.markup('{uniqueId}'))</script>
+
+                <script>
+                    Regate.RegateDropdown.init({{
+                        id: '{uniqueId}',
+                        name: '{props.Name}',
+                        value: '{props.Value}',
+                        isRequired: {props.IsRequired.ToString().ToLower()},
+                        options: {optionsJsonString},
+                    }});
+                </script>
+            ";
+        }
+
+    }
+
+}
