b82319b27b7de6c36e0af677ba7c389d8678597c
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-11-06 13:09:18 +0330

    introduce InteractiveBoolean props

---
+++ src/Regate/InteractiveBoolean/InteractiveBooleanBuild.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Text.Encodings.Web;
+
+namespace Regate
+{
+    public static partial class InteractiveBoolean
+    {
+        private static string _Build(Props props)
+        {
+            var uniqueId = $"RegateText__{props.Name}__{Guid.NewGuid().ToString().Replace("-", "")}";
+
+            var value = string.IsNullOrWhiteSpace(props.Value) ? "" : props.Value;
+            value = JavaScriptEncoder.Default.Encode(value);
+
+
+            return $@"
+                <script>document.write(Regate.RegateText.markup('{uniqueId}'))</script>
+
+                <script>
+                    Regate.RegateText.init({{
+                        id: '{uniqueId}',
+                        name: '{props.Name}',
+                        value: '{value}',
+                        isRequired: {props.IsRequired.ToString().ToLower()},
+                    }});
+                </script>
+            ";
+        }
+
+    }
+
+}
---
+++ src/Regate/InteractiveBoolean/InteractiveBooleanProps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+
+namespace Regate
+{
+    public static partial class InteractiveBoolean
+    {
+        
+        private class Props
+        {
+            public Props(string name)
+            {
+                Name = name;
+            }
+
+            public Props(string name, bool? value) : this(name)
+            {
+                Value = value;
+            }
+
+            public string Name { get; }
+            public bool? Value { get; }
+            public string ApiUrl { get; set; }
+            public bool IsNullable { get; set; }
+        }
+    }
+
+}
