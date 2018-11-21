1faca7a6a834e7001fbd6271733757f02d32ffe1
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-11-21 15:16:24 +0330

    RegateTimePicker : Props

---
+++ src/Regate/RegateTimePicker/RegateTimePickerProps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace Regate
+{
+    public static partial class RegateTimePicker
+    {
+        
+        private class Props
+        {
+            public Props(string name)
+            {
+                Name = name;
+            }
+
+            public Props(string name, TimeSpan? value) : this(name)
+            {
+                Value = value;
+            }
+
+            public string Name { get; }
+            public TimeSpan? Value { get; }
+            public bool IsRequired { get; set; }
+        }
+    }
+
+}
