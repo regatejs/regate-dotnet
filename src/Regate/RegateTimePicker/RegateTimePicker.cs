1cdc03fdccddea9a8897258cd0eacd6f56930c52
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-11-21 15:17:38 +0330

    finalize RegateTimePicker csharp files

---
+++ src/Regate/RegateTimePicker/RegateTimePicker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace Regate
+{
+    public static partial class RegateTimePicker
+    {
+        public static string Build(string name) =>
+            _Build(new Props(name));
+
+        public static string Build(string name, TimeSpan? value) =>
+            _Build(new Props(name, value));
+
+        public static string Build(
+              string name
+            , TimeSpan? value = null
+            , bool isRequired = false
+        ) =>
+            _Build(new Props(name, value)
+            {
+                IsRequired = isRequired,
+            });
+    }
+
+}
---
+++ src/Regate/RegateTimePicker/RegateTimePickerBuild.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Text.Encodings.Web;
+
+namespace Regate
+{
+    public static partial class RegateTimePicker
+    {
+        private static string _Build(Props props)
+        {
+            var uniqueId = $"RegateTimePicker__{props.Name}__{Guid.NewGuid().ToString().Replace("-", "")}";
+
+            
+            return $@"
+                <script>document.write(Regate.RegateTimePicker.markup('{uniqueId}'))</script>
+
+                <script>
+                    Regate.RegateTimePicker.init({{
+                        id: '{uniqueId}',
+                        name: '{props.Name}',
+                        value: '{props.Value}',
+                        isRequired: {props.IsRequired.ToString().ToLower()},
+                    }});
+                </script>
+            ";
+        }
+
+    }
+
+}
