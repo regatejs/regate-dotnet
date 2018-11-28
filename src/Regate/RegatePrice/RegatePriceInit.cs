ccdfb74cc6443923024429c0ad2acebbc00eed4a
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-11-28 11:22:03 +0330

    introduce basic version of RegatePrice

---
+++ src/Regate/RegatePrice/RegatePriceInit.cs
@@ -0,0 +1,13 @@
+﻿namespace Regate
+{
+    public static partial class RegatePrice
+    {
+        public static string Init()
+        {
+            return $@"
+                <script>{_GetJs()}</script>
+            ";
+        }
+    }
+}
+  
