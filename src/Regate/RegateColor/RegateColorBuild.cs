5ab6b09360a3765a81bdddb236e355ad2ab62df2
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-12-25 13:08:17 +0330

    regate color files

---
+++ src/Regate/RegateColor/RegateColorBuild.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Text.Encodings.Web;
+
+namespace Regate
+{
+    public static partial class RegateColor
+    {
+        private static string _Build(Props props)
+        {
+            var uniqueId = $"RegateColor__{props.Name}__{Guid.NewGuid().ToString().Replace("-", "")}";
+
+            
+            return $@"
+                <script>document.write(Regate.RegateColor.markup('{uniqueId}'))</script>
+
+                <script>
+                    Regate.RegateColor.init({{
+                        id: '{uniqueId}',
+                        name: '{props.Name}',
+                        value: '{props.Value}',
+                        isRequired: {props.IsRequired.ToString().ToLower()},
+                    }});
+                </script>
+            ";
+        }
+
+    }
+
+}
